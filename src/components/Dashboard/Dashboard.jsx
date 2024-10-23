283fa733fde6d351973c32c2dd72a4628397d71c
Otabek <burhonovotabek5@gmail.com>
2024-10-23 10:37:50 +0500

    replied functions added

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -7,7 +7,7 @@ import ShareScreen from './ShareScreen';
 const Dashboard = () => {
     return (
 
-        <div style={{ width: "100%", height: "100vh", background: "gray", display: "flex", justifyContent: "end" }}>
+        <div style={{ width: "100%", height: "150vh", background: "gray", display: "flex", justifyContent: "end" }}>
             <LiveChat />
             <Search />
 
